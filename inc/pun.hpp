dee9f0e56bf89d5106d93aea0e5574feca69912b
louisfelix90 <louisfelix@galeota.fr>
2017-04-12 22:44:45 +0200

    Encapsulation de Pun dans une structure, pour apporter des fonctions spécifiques

--- inc/pun.hpp
+++ inc/pun.hpp
@@ -1,15 +1,17 @@
 #ifndef ECE_OTHELLO_PUN_HPP
 #define ECE_OTHELLO_PUN_HPP
 
-#include <iostream>
-#include <string>
-#include <vector>
-
 //enumeration qui permet de savoir si une case est blanche noire ou vide !
-enum Pun {
-	blank = 0,
-	black = 1,
-	white = 2
+struct Pun {
+	enum Colors {
+		blank = 0,
+		black = 1,
+		white = 2
+	};
+
+	static inline Colors opposite( Colors color ) {
+		return (Colors) ( (int) Colors::white - (int) color + (int) Colors::black );
+	}
 };
 
 
