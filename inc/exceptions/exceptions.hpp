9dc869a30c253a1c98b6617b28dd652762743b06
louisfelix90 <louisfelix@galeota.fr>
2017-04-12 22:40:17 +0200

    Ajout d'exceptions

---
+++ inc/exceptions/exceptions.hpp
@@ -0,0 +1,13 @@
+#ifndef _ECE_SIAM_EXCEPTIONS_HPP_
+	#define _ECE_SIAM_EXCEPTIONS_HPP_
+
+	#include <stdexcept>
+	#include <string>
+
+	namespace Othello {
+		namespace exceptions { //exeptions inclusions
+			#include "invalid_move"
+			#include "exit_game"
+		}
+	}
+#endif
