83bfd1b4675a47a2d9236b7d2b8678dd5e7ffc20
louisfelix90 <louisfelix@galeota.fr>
2017-04-12 22:41:08 +0200

    Ajout d'une liste des 8 directions usuelles

--- direction_vector.cpp
+++ direction_vector.cpp
@@ -1,3 +1,9 @@
 #include "inc/direction_vector.hpp"
 
-DirectionVector::DirectionVector( char direction_x, char direction_y ) : x( x ), y( y ) {}
+DirectionVector::DirectionVector( char direction_x, char direction_y ) : x( direction_x ), y( direction_y ) {}
+
+const std::list<DirectionVector> DirectionVector::directions = std::list<DirectionVector>( { DirectionVector( -1, -1 ), DirectionVector( -1, 0 ), DirectionVector( 0, -1 ), DirectionVector( 0, 1 ), DirectionVector( 1, 0 ), DirectionVector( 1, 1 ), DirectionVector( -1, -1 ), DirectionVector( -1, 1 ), DirectionVector( 1, -1 ) } );
+
+bool const operator==( DirectionVector& lhs, DirectionVector& rhs ) {
+	return ( lhs.x == rhs.x && lhs.y == rhs.y );
+}
--- inc/direction_vector.hpp
+++ inc/direction_vector.hpp
@@ -1,16 +1,17 @@
 #ifndef ECE_OTHELLO_DIRECTIONVECTOR_HPP
 #define ECE_OTHELLO_DIRECTIONVECTOR_HPP
 
+#include <list>
 
 struct DirectionVector {
 	const char x;
 	const char y;
 
 	DirectionVector( char direction_x, char direction_y );
+
+	const static std::list<DirectionVector> directions;
 };
 
-bool const operator==( DirectionVector& lhs, DirectionVector& rhs ) {
-	return ( lhs.x == rhs.x && lhs.y == rhs.y );
-}
+bool const operator==( DirectionVector& lhs, DirectionVector& rhs );
 
 #endif //ECE_OTHELLO_DIRECTIONVECTOR_HPP
