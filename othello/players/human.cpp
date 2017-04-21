732384a088275a5da6967973f832361dd70fd82a
Romain <romainbrisse@live.fr>
2017-04-21 10:16:26 +0200

    doxygenation 22

--- othello/players/human.cpp
+++ othello/players/human.cpp
@@ -4,8 +4,15 @@ using namespace std;
 using namespace Othello::Board;
 using namespace Othello::Players;
 
+/**
+* @brief Constructeur surchargé de la classe Human
+**/
 Human::Human( string name, Pun::Colors color ) : UIPlayer( name, color ) {}
 
+/**
+* @brief Accesseur en lecture du mouvement dans la classe Human
+* @return Le mouvement pour l'UI
+**/
 Move Human::getMove() {
 	return m_ui->getMove();
 }
