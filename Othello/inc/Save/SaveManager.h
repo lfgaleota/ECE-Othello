d6a6699321ea042b629794a144b318ee873a73d8
Romain <romainbrisse@live.fr>
2017-04-22 18:46:37 +0200

    implémentation de la classe SaveManager

--- Othello/inc/Save/SaveManager.h
+++ Othello/inc/Save/SaveManager.h
@@ -1,6 +1,26 @@
 #ifndef SAVEMANAGER_H_INCLUDED
 #define SAVEMANAGER_H_INCLUDED
 
+#include <iostream>
+#include "Save.h"
+
+class SaveManager
+{
+private :
+
+    Save firstSlot ;
+    Save secondSlot ;
+    Save thirdSlot ;
+
+public :
+
+    SaveManager();
+    ~SaveManager();
+
+    save();
+    load();
+};
+
 
 
 #endif // SAVEMANAGER_H_INCLUDED
