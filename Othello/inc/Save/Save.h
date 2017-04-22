372440e2c0cf547cfb5c78e55c9c5609b3ff9567
Romain <romainbrisse@live.fr>
2017-04-22 18:18:32 +0200

    recréation de la sauvegarde

---
+++ Othello/inc/Save/Save.h
@@ -0,0 +1,19 @@
+#ifndef SAVE_H_INCLUDED
+#define SAVE_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+
+class SaveManager
+{
+private :
+
+public :
+
+    SaveManager();
+    ~SaveManager();
+};
+
+
+
+#endif // SAVE_H_INCLUDED
