89b83d8b558206f3e13737edc41f7908cc323d89
Romain <romainbrisse@live.fr>
2017-04-22 20:11:20 +0200

    on commente les fonctions de sauvegarde

--- Othello/inc/Save/Save.h
+++ Othello/inc/Save/Save.h
@@ -19,8 +19,11 @@ public :
     /// \fn savePlayer
     void savePlayer(Othello::Players::Human player_toSave, int slot);
 
+    /// \fn loadPlayer
     std::string loadPlayer(int slot);
+    /// \fn loadEmptyNeighbors
     uint64_t loadEmptyNeighbors(int slot);
+    /// \fn loadPunCount
     unsigned char loadPunCount(int slot);
 
     /// \fn destructor
