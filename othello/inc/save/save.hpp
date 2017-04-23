ac050841cfabaeec0f3736a19d162688e2628ed0
Romain <romainbrisse@live.fr>
2017-04-23 17:39:13 +0200

    doxygenation 26

--- othello/inc/save/save.hpp
+++ othello/inc/save/save.hpp
@@ -8,12 +8,18 @@
 	#include "../board/gameboardconstants.hpp"
 	#include "../board/pun.hpp"
 
+	/// \namespace Othello
 	namespace Othello {
+	    /// \namespace Players
 		namespace Players{
+			/// \class Player
 			class Player;
 		}
 
+		/// \namespace Save
 		namespace Save {
+
+		    /// \struct Save
 			struct Save {
 				Othello::Board::Pun::Colors board[ Othello::Board::sizeEdge ][ Othello::Board::sizeEdge ];
 				uint64_t emptyNeighbors;
